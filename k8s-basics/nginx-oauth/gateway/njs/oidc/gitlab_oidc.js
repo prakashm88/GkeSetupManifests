8bf31bf88a8a06d8b8f36a324abd1d62caeed975
Prakash <prakashm88@yahoo.co.in>
2024-07-29 00:17:23 +0530

    validate with new changes

--- k8s-basics/nginx-oauth/gateway/njs/oidc/gitlab_oidc.js
+++ k8s-basics/nginx-oauth/gateway/njs/oidc/gitlab_oidc.js
@@ -112,6 +112,9 @@ function auth_request(r) {
 
 function validate(r) {
   const sessionId = r.headersIn["Cookie"].split("=")[1];
+  r.log("Validating session...");
+  const cookie = r.headersIn["Cookie"] || "";
+  const sessionId = cookie.split("=")[1];
 
   if (!sessionId || !sessions[sessionId]) {
     r.return(401, "Invalid session");
